e0472f114aa42edc83642d2b37c2731543917400
Brandon Boettcher <brandonboettcher19@gmail.com>
2025-07-14 06:55:40 -0500

    Update function names

--- include/ctype.h
+++ include/ctype.h
@@ -1,19 +1,19 @@
 #ifndef __BMBC_CTYPE_H_INCLUDED_
 #define __BMBC_CTYPE_H_INCLUDED_
 
-int my_isalnum(int c);
-int my_isalpha(int c);
-int my_isblank(int c);
-int my_iscntrl(int c);
-int my_isdigit(int c);
-int my_isgraph(int c);
-int my_islower(int c);
-int my_isprint(int c);
-int my_ispunct(int c);
-int my_isspace(int c);
-int my_isupper(int c);
-int my_isxdigit(int c);
-int my_tolower(int c);
-int my_toupper(int c);
+int isalnum(int c);
+int isalpha(int c);
+int isblank(int c);
+int iscntrl(int c);
+int isdigit(int c);
+int isgraph(int c);
+int islower(int c);
+int isprint(int c);
+int ispunct(int c);
+int isspace(int c);
+int isupper(int c);
+int isxdigit(int c);
+int tolower(int c);
+int toupper(int c);
 
 #endif
